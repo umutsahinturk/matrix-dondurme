9644501fa27e0c96ed57e6e6edfca395af872373
umutsahinturk <umut.sahinturk@std.yildiz.edu.tr>
2025-01-12 05:52:08 +0300

    kullanıcıdan matrisin boyutunu almak için gerekli olan bölüm eklendi.

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
-#define N 6
-
-void kullanicidan_matris_al(int arr[N][N]);
-void matrisi_saga_dondur(int arr[N][N]);
-void matrisi_sola_dondur(int arr[N][N]);
+void kullanicidan_matris_al(int n, int arr[n][n]);
+void matrisi_saga_dondur(int n, int arr[n][n]);
+void matrisi_sola_dondur(int n, int arr[n][n]);
 
 int main() 
 {
-    int matris[N][N];
+    int boyut;   
 
-    kullanicidan_matris_al(matris);
+    printf("Matrisin boyutunu girin: ");
+    int hata_kodu = scanf("%d", &boyut);
+
+    if (hata_kodu == EOF || boyut <= 0) {
+        printf("Lütfen geçerli bir sayı değeri girin. (Pozitif tam sayılar)");
+        return 1;
+    }
+
+    int matris[boyut][boyut];
+    kullanicidan_matris_al(boyut, matris);
 
     char dondurulecek_yon;
     
@@ -19,12 +26,12 @@ int main()
 
     if (dondurulecek_yon == '1') 
     {
-        matrisi_saga_dondur(matris);
+        matrisi_saga_dondur(boyut, matris);
     }
 
     else if (dondurulecek_yon == '0') 
     {
-        matrisi_sola_dondur(matris);
+        matrisi_sola_dondur(boyut, matris);
     } 
     
     else {
@@ -34,17 +41,24 @@ int main()
     return 0;
 }
 
-void kullanicidan_matris_al(int arr[N][N])
+void kullanicidan_matris_al(int n, int matris[n][n])
 {
-    
+    for (int i = 1; i <= n; i++) 
+    {
+        for (int j = 1; j <= n; j++) 
+        {
+            printf("Lütfen %dx%d bouyutundaki matrisin %d. satır %d. sütunundaki elemanıni girin: ", n, n, i, j);
+            scanf(" %d", &matris[i][j]);
+        }
+    }
 }
 
-void matrisi_saga_dondur(int arr[N][N]);
+void matrisi_saga_dondur(int n, int arr[n][n])
 {
 printf("slm");
 }
 
-void matrisi_sola_dondur(int arr[N][N]);
+void matrisi_sola_dondur(int n, int arr[n][n])
 {
 printf("slm");
 }
